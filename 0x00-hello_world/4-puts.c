912fcb48931151f23d1187aab339833b46193167
Realyoung1 <olalekano47@gmail.com>
2023-02-16 12:08:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * R<F10><F11>eturn: 0 (Success)
+ * main - This is a program that prints a line with puts function
+ * Return: 0 (Success)
  */
 init main(void)
 {
