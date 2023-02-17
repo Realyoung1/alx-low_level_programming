13bbfef43f375d1df2706e8e4d9df882673f1c92
Realyoung1 <olalekano47@gmail.com>
2023-02-17 15:21:28 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!(ones == '9' && tens == '8'))/*add comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
