e9529de024924061da673d32900934855a32538f
Realyoung1 <olalekano47@gmail.com>
2023-02-17 18:20:34 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+/**
+ * main - Entry point to print all the numbers in a base
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+
+{
+
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
