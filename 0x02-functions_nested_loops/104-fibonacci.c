88c9bd0e9d7f68472c20b8a6c7f59a3e2bff30d5
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-02-22 05:06:47 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 /**
- * main - first 98 Fibonacci numbers
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: program that finds and prints the first 98 Fibonacci
+ *              numbers, starting with 1 and 2
+ *
+ * Return: Always 0 successful
  */
 int main(void)
 {
-	unsigned long int i, a = 0, b = 1, c;
+        unsigned long int t0 = 0, t1 = 1, s;
 
-	for (i = 0; i < 98; i++)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		if (i < 97)
-		{
-			printf("%li, ", c);
-		}
-		else
-		{
-			printf("%li\n", c);
-		}
-	}
-	return (0);
+        int i;
+
+        for (i = 1; i<= 98; i++)
+        {
+                s = t0 + t1;
+
+                if (i != 98)
+                {
+                        printf("%lu, ", s);
+                }
+                else
+                {
+                        printf("%lu\n", s);
+                }
+                t0 = t1;
+                t1 = s;
+        }
+        return (0);
 }
