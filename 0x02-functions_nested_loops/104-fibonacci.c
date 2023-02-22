e9f8276222e5fa05d11e130f27ae9ca4127c8882
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-02-22 05:16:18 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,24 +9,23 @@
  */
 int main(void)
 {
-        unsigned long int t0 = 0, t1 = 1, s;
+unsigned long int t0 = 0, t1 = 1, s;
+int i;
 
-        int i;
+for (i = 1; i<= 98; i++)
+{
+s = t0 + t1;
 
-        for (i = 1; i<= 98; i++)
-        {
-                s = t0 + t1;
-
-                if (i != 98)
-                {
-                        printf("%lu, ", s);
-                }
-                else
-                {
-                        printf("%lu\n", s);
-                }
-                t0 = t1;
-                t1 = s;
-        }
-        return (0);
+if (i != 98)
+{
+printf("%lu, ", s);
+}
+else
+                
+printf("%lu\n", s);
+}
+t0 = t1;
+t1 = s;
+}
+return (0);
 }
