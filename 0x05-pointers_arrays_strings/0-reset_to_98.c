99db7f4631071be216e0447a203f3e1679576d28
Realyoung1 <olalekano47@gmail.com>
2023-02-27 07:36:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - to take a pointer to an int as parameter and updates the value
+*  @n: int to check
+*  Owned By RealDevices Tech/Realyoung1
+*  Return: Always 0 (success)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
