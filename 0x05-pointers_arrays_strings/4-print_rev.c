5b59422aa82be5dc1a40d097bab9808670b09355
Realyoung1 <olalekano47@gmail.com>
2023-02-27 07:54:08 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_rev -> function that prints a string, in reverse, followed by a new line
+* print_rev - function that prints a string, in reverse, followed by a new line
 * @s: the string to be printed in rev
 */
 
