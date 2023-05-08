fbb76400ccda506a140b825264aaef8b2633cbe7
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-08 06:03:36 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,15 +2,15 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif
