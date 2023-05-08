cbf1a134797973fab59ec70dff9dfbee9b262c36
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-08 06:04:03 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
