0a0d8616f8f2edcbfe133b5530afd1698f14e196
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 11:01:50 -0700

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,24 @@
 #include "lists.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
 /**
- * free_dlistint - free a dlistint_t list
- * @head: pointer to the beginning of the linked list
+ * free_dlistint - function that free a dlistint_t list.
+ * @head: pointers to the beginning of the linked lists.
  *
  * Return: void
  */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *next;
