68ed4dd9e066ccd04fb79ba75ab77d8ce1249abd
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 11:04:53 -0700

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
 /**
- * sum_dlistint - sums all of the data of a dlistint_t linked list
- * @head: pointer to the beginning of the linked list
+ * sum_dlistint - func that sums all of the data of a dlistint_t linked list
+ * @head: pointers to the beginning of the linked lists.
  *
  * Return: sum of all data, or 0 if the list is empty
  */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
