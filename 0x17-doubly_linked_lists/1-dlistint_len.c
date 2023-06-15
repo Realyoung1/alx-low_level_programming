d1e82ede0237d8b979b928dff2a3f485de5c3305
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 10:57:21 -0700

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,28 @@
 #include "lists.h"
 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
 /**
- * dlistint_len - count the number of nodes in the linked list
- * @h: pointer to the beginning of a linked list
+ * dlistint_len - func counts the number of nodes in the linked list.
+ * @h: pointer to the beginning of a linked lists.
  *
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+	size_t k;
 
-	for (i = 0; h != NULL; i++)
+	for (k = 0; h != NULL; k++)
 		h = h->next;
-	return (i);
+	return (k);
 }
