44727261bd3be356a85c2e42aa5e0c65bed9f9af
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 10:58:49 -0700

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,9 +1,20 @@
 #include "lists.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
 /**
- * add_dnodeint - add a new node at the beginning of the linked list
- * @head: double pointer to the beginning of the linked list
+ * add_dnodeint - to add a new node at the beginning of the linked lists.
+ * @head: double pointer to the beginning of the linked lists.
  * @n: value to add to the new node
  *
  * Return: pointer to the new node, or NULL on failure
