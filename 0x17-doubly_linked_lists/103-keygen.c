7d2f9efa04f37d9135f550d33a7d2abd6a51a4ab
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 11:16:05 -0700

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
+ * main - to generate a key depending on a username for crackme5
+ * @argc: numbers of arguments passed.
+ * @argv: arguments passed to main.
  *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
+	unsigned int k, b;
 	size_t len, add;
 	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
@@ -23,21 +31,21 @@ int main(int argc, char *argv[])
 	}
 	len = strlen(argv[1]);
 	p[0] = l[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
+	for (k = 0, add = 0; k < len; k++)
+		add += argv[1][k];
 	p[1] = l[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
+	for (k = 0, b = 1; k < len; k++)
+		b *= argv[1][k];
 	p[2] = l[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
+	for (b = argv[1][0], k = 0; k < len; k++)
+		if ((char)b <= argv[1][k])
+			b = argv[1][k];
 	srand(b ^ 14);
 	p[3] = l[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
+	for (b = 0, k = 0; k < len; k++)
+		b += argv[1][k] * argv[1][k];
 	p[4] = l[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+	for (b = 0, k = 0; (char)k < argv[1][0]; k++)
 		b = rand();
 	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
