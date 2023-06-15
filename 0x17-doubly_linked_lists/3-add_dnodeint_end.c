d058ffe6a8f5b0442bc9e6cbb5dd2a04d395b088
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 11:00:26 -0700

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,13 +1,25 @@
 #include "lists.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
 /**
- * add_dnodeint_end - adds a node to the end of a linked list
- * @head: double pointer to the beginning of the linked list
+ * add_dnodeint_end - func adds a node to the end of a linked list.
+ * @head: this double pointer to the beginning of the linked list.
  * @n: value to add to new node
  *
  * Return: pointer to the new node, or NULL on failure
  */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *tmp;
