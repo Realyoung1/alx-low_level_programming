1b2464ac1a1b0597edebbbc036f4bc77f057562a
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-15 11:07:33 -0700

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,10 +1,20 @@
 #include "lists.h"
-#include <stdlib.h>
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: double pointer to the beginning of the linked list
+ * insert_dnodeint_at_index - func inserts a new node at a given position.
+ * @h: doubles pointers to the beginning of the linked list.
  * @idx: index at which to insert the new node
  * @n: data to enter into new node
  *
@@ -13,14 +23,14 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *next, *current;
-	unsigned int i;
+	unsigned int k;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx != 0)
 	{
 		current = *h;
-		for (i = 0; i < idx - 1 && current != NULL; i++)
+		for (k = 0; k < idx - 1 && current != NULL; k++)
 			current = current->next;
 		if (current == NULL)
 			return (NULL);
