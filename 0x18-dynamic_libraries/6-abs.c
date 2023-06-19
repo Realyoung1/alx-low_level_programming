bbaea478374d6a9bb2853aa156c51644f0275d50
Realyoung1 <olalekano47@gmail.com>
2023-06-19 14:42:37 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer.
+ * @t: The integer to be computed
+ * Return: The absolute value
+ */
+
+int _abs(int t)
+
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
