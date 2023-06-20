b2b43a9c8d819f8e30a154c80081153a083458d1
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-20 06:10:44 -0700

    Update 100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,4 +1,14 @@
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * add - addition
