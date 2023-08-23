29fb96937b230996378a34e4126507e764382230
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-23 22:29:13 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-    Function that returns the perimeter\
-    of the island described in grid
+    Functs that return the perimetr\
+    of the island describ in a grid
 """
 
 
 def island_perimeter(grid):
-    """" Returns the perimeter of the\
-    island described in grid"""
+    """" Returning the perimetr of the\
+    island describ in a grid"""
     if not len(grid):
         return 0
 
     perimeter = 0
-    for i, g_cell in enumerate(grid):
-        for j, cell in enumerate(g_cell):
+    for v, g_cell in enumerate(grid):
+        for m, cell in enumerate(g_cell):
             if cell:
                 perimeter += 4
-                if i != 0 and grid[i - 1][j] == 1:
+                if v != 0 and grid[v - 1][m] == 1:
                     perimeter -= 2
-                if j + 1 != len(g_cell) and g_cell[j + 1] == 1:
+                if m + 1 != len(g_cell) and g_cell[m + 1] == 1:
                     perimeter -= 2
 
     return perimeter
