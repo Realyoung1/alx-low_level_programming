6273640070eb7e1107dbabe15709bc90f6e1a7b2
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-09 07:30:17 -0700

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,20 @@
-#ifndef SEARCH_ALGO
-#define SEARCH_ALGO
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 
+/* LIBRARIES */
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
 
+/* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
