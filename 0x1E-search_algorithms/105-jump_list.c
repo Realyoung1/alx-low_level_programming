4d70b46d33d140e6adab3aa3144e2a37d775e307
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-09 07:25:51 -0700

    find algo

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,18 +1,24 @@
 #include "search_algos.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * jump_list - Searches for an algorithm in a sorted singly
- *             linked list of integers using jump search.
  * @list: A pointer to the  head of the linked list to search.
  * @size: The number of nodes in the list.
  * @value: The value to search for.
- *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
  */
+
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t step, step_size;
