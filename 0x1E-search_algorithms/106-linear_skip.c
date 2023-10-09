686cba9c7948815d217fec7bdd195ff1c3e8b414
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-09 07:26:57 -0700

    find algo

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,17 +1,24 @@
 #include "search_algos.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * linear_skip - Searches for an algorithm in a sorted singly
- *               linked list of integers using linear skip.
  * @list: A pointer to the  head of the linked list to search.
  * @value: The value to search for.
  *
  * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *node, *jump;
