40fff4671bfcc098ea5d4b00a43459c24488f7e3
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-09 07:13:23 -0700

    find algo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,12 +1,24 @@
 #include "search_algos.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
-  * print_array - print array with limits in l and r
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * Return: nothing
+  * print_array - printed arrays with limits in l and r
+  * @array: setted of numbered
+  * @l: left limits
+  * @r: right limits
+  * Return: nothingss
   */
+
 void print_array(int *array, size_t l, size_t r)
 {
 	printf("Searching in array: ");
@@ -15,14 +27,14 @@ void print_array(int *array, size_t l, size_t r)
 	printf("%d\n", array[l]);
 }
 
-
 /**
-  * binary_search - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @size: size of the array
-  * @value: value to search
+  * binary_search - searched the first ocurrencies of a valued in the arrays
+  * @array: setted of numbered
+  * @size: sized of the arrays
+  * @value: values to searches
   * Return: return the first index located otherwise -1
   */
+
 int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, r = size - 1, mid = 0;
