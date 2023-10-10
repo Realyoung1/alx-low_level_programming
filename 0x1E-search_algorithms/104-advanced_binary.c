e8c52eea941018de6bf87a2569a92e7e690ab14d
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-10 09:57:11 -0700

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -12,18 +12,13 @@
 #include <stdlib.h>
 
 /**
-  * advanced_binary_recursive - Searches recursively for a value in a sorted
-  *                             array of integers using binary search.
-  * @array: A pointer to the first element of the [sub]array to search.
-  * @left: The starting index of the [sub]array to search.
-  * @right: The ending index of the [sub]array to search.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present, -1.
-  *         Otherwise, the index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
+  * print_array - printed array with limits in l and r
+  * @array: setted of numbered
+  * @l: left limitss
+  * @r: right limitss
+  * Return: nothingss
   */
+
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
@@ -45,21 +40,60 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 }
 
 /**
-  * advanced_binary - Searches for a value in a sorted array
-  *                   of integers using advanced binary search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
+  * bi_se - searched the first ocurrency of a value in the array.
+  * @array: set of numbers
+  * @l: lefted limitss
+  * @r: rightee limitss
+  * @value: value to search
+  * Return: return the first index located otherwise -1
   */
+
+size_t bi_se(int *array, size_t l, size_t r, int value)
+{
+	int mid = 0, index = -1;
+
+	if (l > r)
+		return (-1);
+
+	print_array(array, l, r);
+	mid = (l + r) / 2;
+	if (array[mid] < value)
+	{
+		l = mid + 1;
+		index = bi_se(array, l, r, value);
+	}
+	else if (array[mid] > value)
+	{
+		r = mid - 1;
+		index = bi_se(array, l, r, value);
+	}
+	else
+		if (mid - 1 >= 0 && array[mid - 1] == array[mid])
+			index = bi_se(array, l, mid, value);
+		else
+			index = mid;
+
+	return (index);
+}
+
+/**
+  * advanced_binary - searched the first ocurrency of a values.
+  * @array: set of numberse
+  * @size: sized of the arrays
+  * @value: valued to searches
+  * Return: return the first index located otherwise -1
+  */
+
 int advanced_binary(int *array, size_t size, int value)
 {
+	int index = -1;
+
 	if (array == NULL || size == 0)
 		return (-1);
 
-	return (advanced_binary_recursive(array, 0, size - 1, value));
+	index = bi_se(array, 0, size - 1, value);
+
+	if (index >= 0)
+		return (index);
+	return (-1);
 }
