a0a51e211154f4ff73f14915f15ac89956864e9e
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-10 10:06:47 -0700

    find algo

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -14,49 +14,43 @@
 
 /**
   * advanced_binary_recursive - Searches recursively for a value in a sorted
-  *                             array of integers using binary search.
   * @array: A pointer to the first element of the [sub]array to search.
   * @left: The starting index of the [sub]array to search.
   * @right: The ending index of the [sub]array to search.
   * @value: The value to search for.
-  *
   * Return: If the value is not present, -1.
-  *         Otherwise, the index where the value is located.
-  *
   * Description: Prints the [sub]array being searched after each change.
   */
+
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-	size_t i;
+	size_t q;
 
 	if (right < left)
 		return (-1);
 
 	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+	for (q = left; q < right; q++)
+		printf("%d, ", array[q]);
+	printf("%d\n", array[q]);
 
-	i = left + (right - left) / 2;
-	if (array[i] == value && (i == left || array[i - 1] != value))
-		return (i);
-	if (array[i] >= value)
-		return (advanced_binary_recursive(array, left, i, value));
-	return (advanced_binary_recursive(array, i + 1, right, value));
+	q = left + (right - left) / 2;
+	if (array[q] == value && (q == left || array[q - 1] != value))
+		return (q);
+	if (array[q] >= value)
+		return (advanced_binary_recursive(array, left, q, value));
+	return (advanced_binary_recursive(array, q + 1, right, value));
 }
 
 /**
   * advanced_binary - Searches for a value in a sorted array
-  *                   of integers using advanced binary search.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
+  * @value: The value to search for
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
   * Description: Prints the [sub]array being searched after each change.
   */
+
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
