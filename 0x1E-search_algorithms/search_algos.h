1df56fb36359d5e78cb57ecf75d5b888304274fe
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-10 09:32:20 -0700

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,17 @@
 #define _SEARCH_ALGOS_H_
 
 /* LIBRARIES */
-#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <math.h>
 
 /* STRUCTURES */
